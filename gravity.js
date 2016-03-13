01d50c2dedfa6899905670b315a2c21534eb2fb5
Haurentziu Haurel <regeleromaniei@aol.com>
2016-03-13 16:04:51 +0200

    fixed firefox problems, added velocity vector

--- gravity.js
+++ gravity.js
@@ -3,15 +3,23 @@ var ctx = c.getContext("2d");
 
 resizeCanvas();
 
-c.addEventListener("mousedown", getClickPosition, false)
+c.addEventListener("mousedown", getDownPosition, false);
+c.addEventListener("mouseup", getUpPosition, false);
+c.addEventListener("mousemove", getMovePosition, false);
+
 ctx.fillStyle = "#000000";
 
 
 //Universe proprities
-var k = 0.8; //Universal gravitational constant
-var step = 10; //The step of the simulation
+var k = 0.7; //Universal gravitational constant
+var fps = 40; //fps
+var step = 2; //the step of the simulation
+var smoothingFactor = .01;
 
-var trailLength = 100;
+var trailLength = 500;
+
+var beginX, beginY, endX = 0, endY = 0;
+var isPressed = false;
 
 var colors = [
 	"#e57373",
@@ -36,12 +44,14 @@ var colors = [
 
 var bodies = [
 	new Body(100, window.innerWidth/2, window.innerHeight/2,  0, 0, colors[0], 20),
-	new Body(.1, window.innerWidth/2, window.innerHeight/2 - 150, .6, 0, colors[4], 10),
+	new Body(0, window.innerWidth/2, window.innerHeight/2 - 150, .6, 0, colors[4], 10),
 ];
  
 
 window.onload = function(){
-	setInterval(drawBodies, 10);
+	tooglePanel();
+	setInterval(drawBodies, 1000/fps);
+	setInterval(computePosition, step);
 
 }
 
@@ -52,9 +62,21 @@ function pickColor(){
 function drawBodies(){
 	ctx.fillStyle = "#000000";
 	ctx.fillRect(0, 0, c.width, c.height);
-//	ctx.clearRect(0, 0, c.width, c.height);
-	computePosition();
+
+	if(isPressed){
+		ctx.lineWidth = 1;
+		ctx.strokeStyle = "#FFFFFF";
+		var alpha = Math.atan2(beginY - endY, beginX - endX);
+		ctx.moveTo(beginX, beginY);
+		ctx.lineTo(endX, endY);
+		ctx.lineTo(endX - 30*Math.sin(-alpha - Math.PI/3), endY - 30*Math.cos(-alpha - Math.PI/3));
+		ctx.moveTo(endX, endY);
+		ctx.lineTo(endX - 30*Math.sin(Math.PI - alpha + Math.PI/3), endY - 30*Math.cos(Math.PI - alpha + Math.PI/3));
+		ctx.stroke();
+	}
+
 	for(var i = 0; i < bodies.length; i++){
+
 		ctx.fillStyle = bodies[i].color;
 		ctx.strokeStyle = bodies[i].color;
 		ctx.beginPath();
@@ -66,6 +88,7 @@ function drawBodies(){
 }
 
 function drawBodyTrail(bodyId){
+	ctx.lineWidth = 2;
 	var length = bodies[bodyId].positionsX.length-1;
 	ctx.beginPath();
 	for(var i = 0; i < length; i++){
@@ -83,7 +106,7 @@ function computePosition(){
 			if(i != j){
 				var deltaX = bodies[j].x - bodies[i].x;
 				var deltaY = -bodies[i].y + bodies[j].y;
-				var d = Math.sqrt(deltaX*deltaX + deltaY*deltaY);
+				var d = Math.sqrt(deltaX*deltaX + deltaY*deltaY) + smoothingFactor;
 				var acc = k*bodies[j].mass/(d*d);
 				accX += acc*deltaX/d;
 				accY += acc*deltaY/d;
@@ -99,22 +122,34 @@ function computePosition(){
 	}
 }
 
-function getClickPosition(e){
-	var x = e.x;
-	var y = e.y;
+function getDownPosition(e){
+	var x = e.clientX - c.offsetLeft;
+	var y = e.clientY - c.offsetTop;
 
-	x -= c.offsetLeft;
-	y -= c.offsetTop;
-
-	var deltaX = x - bodies[0].x;
-	var deltaY = bodies[0].y - y;
-	var d = Math.sqrt(deltaX*deltaX + deltaY*deltaY);
-	var vel = Math.sqrt(k * bodies[0].mass / d);
-	var velX = vel * deltaY/d;
-	var velY = vel * deltaX/d;
-	var body = new Body(0, x, y , velX, velY, colors[pickColor()], 10);
-	bodies.push(body);
+	isPressed = true;
+	beginX = endX = x;
+	beginY = endY = y;
 }
+
+function getMovePosition(e){
+	if(isPressed){
+		endX = e.clientX - c.offsetLeft;
+		endY = e.clientY - c.offsetTop;
+	}
+}
+
+function getUpPosition(e){
+	isPressed = false;
+	endX = e.clientX - c.offsetLeft;
+	endY = e.clientY - c.offsetTop;
+	var velX = (endX - beginX)/500;
+	var velY = (endY - beginY)/500;
+	body = new Body(0, beginX, beginY, velX, velY, colors[pickColor()], 10);
+	bodies.push(body);
+	console.log("Created body");
+	console.log(bodies[0]);
+}
+
 
 function resizeCanvas(){
 	ctx.canvas.width = window.innerWidth;
