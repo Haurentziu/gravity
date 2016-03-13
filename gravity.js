a1e039574184d97a4a5b49aeefe7642bb1ecbd64
Haurentziu Haurel <Haurentziu@users.noreply.github.com>
2016-03-13 18:32:08 +0200

    changed background color

--- gravity.js
+++ gravity.js
@@ -40,6 +40,24 @@ var colors = [
 	"#FF8A65",
 	"#A1887F",
 	"#90A4AE",
+	/////////
+/*	"#b71c1c",
+	"#880E4F",
+	"#4A148C",
+	"#311B92",
+	"#1A237E",
+	"#0D47A1",
+	"#01579B",
+	"#006064",
+	"#004D40",
+	"#1B5E20",
+	"#33691E",
+	"#827717",
+	"#F57F17",
+	"#FF6F00",
+	"#E65100",
+	"#BF360C",*/
+
 ];
 
 var bodies = [
@@ -60,7 +78,7 @@ function pickColor(){
 }
 
 function drawBodies(){
-	ctx.fillStyle = "#000000";
+	ctx.fillStyle = "#111111";
 	ctx.fillRect(0, 0, c.width, c.height);
 
 	if(isPressed){
