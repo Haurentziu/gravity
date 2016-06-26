0a271d822221e287cb8ebdc07b2125f3dab5627a
Haurentziu <Haurentziu@users.noreply.github.com>
2016-06-26 20:46:53 +0300

    added dat.gui, fixed euler integration etc..

--- body.js
+++ body.js
@@ -6,6 +6,8 @@ function Body(mass, x, y, velocityX, velocityY, color, size){
 	this.velocityY = velocityY;
 	this.positionsX = [];
 	this.positionsY = [];
+	this.accelerationX = 0;	
+	this.accelerationY = 0;
 	this.color = color;
 	this.size = size;
 }
@@ -22,3 +24,34 @@ Body.prototype.addPosition = function(){
 	}
 
 };
+
+Body.prototype.update = function(step){
+	this.velocityX += step * this.accelerationX;
+	this.velocityY += step * this.accelerationY;
+
+	this.x += this.velocityX * step;
+	this.y += this.velocityY * step;
+}
+
+
+Body.prototype.resetAcceleration = function(){
+	this.accelerationY = 0;
+	this.accelerationX = 0;
+}
+
+Body.prototype.setPositions = function(positionsX, positionsY){
+	this.positionsX = positionsX;
+	this.positionsY = positionsY;
+}
+
+Body.prototype.setMass = function(mass){
+	this.mass = mass;
+}
+
+Body.prototype.distanceToBody = function(body){
+	return Math.sqrt((this.x - body.x) * (this.x - body.x) + (this.y - body.y) * (this.y - body.y));
+} 
+
+Body.prototype.distanceTo = function(posX, posY){
+	return Math.sqrt((this.x - posX) * (this.x - posX) + (this.y - posY) * (this.y - posY));
+}
--- gravity.js
+++ gravity.js
@@ -9,17 +9,33 @@ c.addEventListener("mousemove", getMovePosition, false);
 
 ctx.fillStyle = "#000000";
 
+var FPS_MEAN_FRAMES = 20;
 
 //Universe proprities
 var k = 0.7; //Universal gravitational constant
 var fps = 40; //fps
-var step = 2; //the step of the simulation
-var smoothingFactor = .01;
 
-var trailLength = 500;
+var timePerStep = 0.5; //biger -> less accurate
+var stepsPerFrame = 15; //biger -> faster
+var smoothingFactor = .1;
+
+var lastUnix;
+var cycles = 0;
+
+var zoom = 1;
+
+var trailLength = 2000;
+var bodyMass = 10;
 
 var beginX, beginY, endX = 0, endY = 0;
 var isPressed = false;
+
+var selectedBody = 0;
+var isSelected = false;
+
+
+var offsetX = 0;
+var offsetY = 0;
 
 var colors = [
 	"#e57373",
@@ -40,36 +56,37 @@ var colors = [
 	"#FF8A65",
 	"#A1887F",
 	"#90A4AE",
-	/////////
-/*	"#b71c1c",
-	"#880E4F",
-	"#4A148C",
-	"#311B92",
-	"#1A237E",
-	"#0D47A1",
-	"#01579B",
-	"#006064",
-	"#004D40",
-	"#1B5E20",
-	"#33691E",
-	"#827717",
-	"#F57F17",
-	"#FF6F00",
-	"#E65100",
-	"#BF360C",*/
-
 ];
 
-var bodies = [
-	new Body(100, window.innerWidth/2, window.innerHeight/2,  0, 0, colors[0], 20),
-	new Body(0, window.innerWidth/2, window.innerHeight/2 - 150, .6, 0, colors[4], 10),
-];
- 
 
 window.onload = function(){
-	tooglePanel();
-	setInterval(drawBodies, 1000/fps);
-	setInterval(computePosition, step);
+	reset();
+//	createProtoDisk();
+	drawBodies();
+
+	var obj = {
+		mass: 5,
+
+		timePerStep: 0.5,
+		stepsPerFrame: 15,
+
+	};
+
+    var gui = new dat.GUI();
+    var objectFolder = gui.addFolder("Object Settings");
+    var worldFolder = gui.addFolder("World Settings");
+
+    objectFolder.open();
+    worldFolder.open();
+
+    objectFolder.add(this, "bodyMass").min(0).max(150).step(0.1);
+
+	worldFolder.add(this, 'timePerStep').min(0).max(5).step(0.1);
+	worldFolder.add(this, 'stepsPerFrame').min(0).max(30).step(0.1);
+	worldFolder.add(this, 'k').min(0).max(2).step(0.01);
+	worldFolder.add(this, 'smoothingFactor').min(0).max(1).step(0.001);
+	worldFolder.add(this, 'trailLength').min(0).max(3000).step(1);
+	worldFolder.add(this, 'reset');
 
 }
 
@@ -78,6 +95,28 @@ function pickColor(){
 }
 
 function drawBodies(){
+	if(isSelected){
+		offsetX = c.width/2 - bodies[selectedBody].x;
+		offsetY = c.height/2 - bodies[selectedBody].y;
+	}
+
+
+	if(cycles == FPS_MEAN_FRAMES){
+		var fps = Math.floor(FPS_MEAN_FRAMES * 1000 / (Date.now() - lastUnix));
+		lastUnix = Date.now();
+		writeFPS(fps);
+		cycles = 0;
+	}
+	else{
+		cycles++
+	}
+
+	for(var i = 0; i < stepsPerFrame; i++){
+		checkColisions();
+		computePosition();
+	}
+
+
 	ctx.fillStyle = "#111111";
 	ctx.fillRect(0, 0, c.width, c.height);
 
@@ -93,16 +132,26 @@ function drawBodies(){
 		ctx.stroke();
 	}
 
+
 	for(var i = 0; i < bodies.length; i++){
 
 		ctx.fillStyle = bodies[i].color;
 		ctx.strokeStyle = bodies[i].color;
 		ctx.beginPath();
-		ctx.arc(bodies[i].x,bodies[i].y,bodies[i].size,0,2*Math.PI);
+		ctx.arc(offsetX + bodies[i].x, offsetY + bodies[i].y, bodies[i].size, 0, 2*Math.PI);
 		ctx.fill();
 		drawBodyTrail(i);
 	}
 
+	requestAnimationFrame(drawBodies);
+
+}
+
+
+function writeFPS(fps){
+	document.getElementById("fps").innerHTML = "FPS: " + fps;
+	document.getElementById("number").innerHTML = "Number of Bodies: " + bodies.length;
+	
 }
 
 function drawBodyTrail(bodyId){
@@ -110,43 +159,112 @@ function drawBodyTrail(bodyId){
 	var length = bodies[bodyId].positionsX.length-1;
 	ctx.beginPath();
 	for(var i = 0; i < length; i++){
-		ctx.moveTo(bodies[bodyId].positionsX[i],bodies[bodyId].positionsY[i]);
-		ctx.lineTo(bodies[bodyId].positionsX[i + 1],bodies[bodyId].positionsY[i + 1]);
+		ctx.moveTo(offsetX + bodies[bodyId].positionsX[i], offsetY  + bodies[bodyId].positionsY[i]);
+		ctx.lineTo(offsetX + bodies[bodyId].positionsX[i + 1], offsetY + bodies[bodyId].positionsY[i + 1]);
 	}
 	ctx.stroke();
 }
 
+function cloneBodies(array){
+	var returnedArray = [];
+	for(var  i = 0; i < array.length; i++){
+		body = new Body(array[i].mass, array[i].x, array[i].y, array[i].velocityX, array[i].velocityY, array[i].color, array[i].size);
+		body.setPositions(array[i].positionsX, array[i].positionsY);
+		returnedArray.push(body);
+	}
+	return returnedArray;
+}
 
 function computePosition(){
 	for(var  i = 0; i < bodies.length; i++){
-		var accX = 0, accY = 0;
+		bodies[i].resetAcceleration();
 		for(var j = 0; j < bodies.length; j++){
 			if(i != j){
 				var deltaX = bodies[j].x - bodies[i].x;
 				var deltaY = -bodies[i].y + bodies[j].y;
 				var d = Math.sqrt(deltaX*deltaX + deltaY*deltaY) + smoothingFactor;
 				var acc = k*bodies[j].mass/(d*d);
-				accX += acc*deltaX/d;
-				accY += acc*deltaY/d;
+				bodies[i].accelerationX += acc*deltaX/d;
+				bodies[i].accelerationY += acc*deltaY/d;
 			}
-			 
 		}
-		bodies[i].velocityX += accX*step;
-		bodies[i].velocityY += accY*step;
+	
+	}
 
-		bodies[i].x += bodies[i].velocityX*step;
-		bodies[i].y += bodies[i].velocityY*step;
+	for(var i = 0; i < bodies.length; i++){
+		bodies[i].update(timePerStep);
 		bodies[i].addPosition();
+	}
+
+}
+
+function checkColisions(){
+	for(var i = 0; i < bodies.length; i++){
+		for(var j = 0; j < bodies.length; j++){
+			if(i!=j && bodies[i].distanceToBody(bodies[j]) < bodies[i].size + bodies[j].size){
+				console.log("Colision!")
+				var isSelectedBody = (selectedBody == i || selectedBody == j);
+				if(bodies[i].mass > bodies[j].mass){
+					bodies[i].mass += bodies[j].mass;
+					if(isSelectedBody) selectedBody = i;
+				//	bodies[i].size += bodies[j].size;
+					bodies.splice(j, 1);
+				}
+				else{
+					bodies[j].mass += bodies[i].mass;
+					if(isSelectedBody) selectedBody = j;
+				//	bodies[j].size += bodies[i].size;
+					bodies.splice(i, 1);
+				}
+			}
+
+		}
+	}
+}
+
+
+
+function createProtoDisk(){
+	for(var  i = 0; i < 1000; i++){
+		var x = Math.random() * c.width;
+		var y = Math.random() * c.height;
+
+	//	var d = Math.sqrt((c.width / 2 - x) * (c.width / 2 - x) + (c.height / 2 - y) * (c.height / 2 - y));
+	//	var alpha = Math.atan2(c.height/2 - y, c.width / 2 - x);
+	//	var vel = Math.sqrt(k * (bodies[0].mass) / d);
+
+	//	var velX = vel * Math.sin(alpha);
+	//	var velY = -vel * Math.cos(alpha);
+
+		var velX = Math.random() / 50 - 1 / 100;
+		var velY = Math.random() / 50 - 1 / 100;
+
+		var mass = Math.random() * 10;
+
+		body = new Body(mass, x, y, velX, velY, colors[pickColor()], mass);
+		bodies.push(body);
 	}
 }
 
 function getDownPosition(e){
 	var x = e.clientX - c.offsetLeft;
 	var y = e.clientY - c.offsetTop;
+	if(e.which == 1){
+		isPressed = true;
+		beginX = endX = x;
+		beginY = endY = y;
+	}
 
-	isPressed = true;
-	beginX = endX = x;
-	beginY = endY = y;
+	if(e.which == 3){
+		for(var i = 0; i < bodies.length; i++){
+			if(bodies[i].distanceTo(x - offsetX, y - offsetY) < bodies[i].size){
+				console.log(i);
+				selectedBody = i;
+				isSelected = true;
+				break;
+			}
+		}
+	}
 }
 
 function getMovePosition(e){
@@ -157,15 +275,15 @@ function getMovePosition(e){
 }
 
 function getUpPosition(e){
-	isPressed = false;
 	endX = e.clientX - c.offsetLeft;
 	endY = e.clientY - c.offsetTop;
-	var velX = (endX - beginX)/500;
-	var velY = (endY - beginY)/500;
-	body = new Body(0, beginX, beginY, velX, velY, colors[pickColor()], 10);
-	bodies.push(body);
-	console.log("Created body");
-	console.log(bodies[0]);
+	if(e.which == 1){
+		isPressed = false;
+		var velX = (endX - beginX)/500;
+		var velY = (endY - beginY)/500;
+		body = new Body(bodyMass, beginX - offsetX, beginY - offsetY, velX, velY, colors[pickColor()], 10);
+		bodies.push(body);
+	}
 }
 
 
@@ -173,3 +291,22 @@ function resizeCanvas(){
 	ctx.canvas.width = window.innerWidth;
 	ctx.canvas.height = window.innerHeight;
 }
+
+
+function rad2deg(angle){
+	return angle * 180 / Math.PI;
+}
+
+function reset(){
+	bodies = [
+		new Body(100, window.innerWidth/2, window.innerHeight/2,  0, 0, colors[0], 18),
+		new Body(0, window.innerWidth/2, window.innerHeight/2 - 150, .6, 0, colors[4], 10),
+	];
+}
+
+window.onkeydown = function (e) {
+    var code = e.keyCode ? e.keyCode : e.which;
+    if (code === 32) { 
+        isSelected = false;
+    } 
+};
